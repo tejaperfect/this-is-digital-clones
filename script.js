d0602bbaeffba7fad957f6ef2a13feb167d68f2d
omkar barkale <tejaperfect1703@gmail.com>
2025-06-25 14:09:29 +0530

    This is digital

---
+++ script.js
@@ -0,0 +1,32 @@
+gsap.from('#img1', {
+  delay: 0.5,
+  opacity: 0,
+  duration: 2,
+  y: 60
+})
+
+gsap.from('#img2', {
+  delay: 0.5,
+  opacity: 0,
+  duration: 2,
+  x: 60
+})
+
+gsap.from('#img3', {
+  delay: 0.5,
+  opacity: 0,
+  duration: 2,
+  y: -60
+})
+
+gsap.from('#main h1', {
+  delay: 0.5,
+  opacity: 0,
+  duration: 2
+})
+
+gsap.from('#more h3', {
+  delay: 0.5,
+  opacity: 0,
+  duration: 2
+})
